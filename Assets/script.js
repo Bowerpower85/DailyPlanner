2f9d6e004a4256a1060f9ad2066bfafa9c64a5e9
Jonathan Bower <bowerpower85@gmail.com>
2020-08-10 08:37:46 -0700

    Added moment().format to get the current date at the top of the screen.

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,29 @@
-// Display the current day at the top using moment.js
-$("#currentDay").text(moment()).format();
+// $(document).ready(funtion()) {
+//     const currentDay = moment();
+//     console.log(currentDay)
+
+
+// // // Display the current day at the top using moment.js
+//     // $("#currentDay").text(moment()).format("");
+
+// // Add click event to save description to localStorage
+//     $(".saveBtn").on("click", function() {
+//         event.preventDefault();
+//         const task = $(this).siblings(".task").val();
+//         const time = $(this).parent().attr("id");
+//         localStorage.setItem(time, task);
+//     });
+
+// })
+
+$(document).ready(function() {
+    const currentDay = moment().format("dddd, MMMM Do YYYY h:mm A");
+    console.log(currentDay)
+    $("#currentDay").append(currentDay);
+
+
+    
+})
+
+
+
